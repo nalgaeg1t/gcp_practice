43f2229c829f892835bf351f5b5fd7c04b5c6bff
nalgaeg1t <49203563+nalgaeg1t@users.noreply.github.com>
2021-06-12 15:54:08 +0900

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -14,6 +14,7 @@ router.get('/test', function(req, res, next) {
 })
 
 router.get('/test2', function(req, res, next) {
+  console.log(teacher.read('test_id'))
   res.json(teacher.read('test_id'));
 })
 
