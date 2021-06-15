4b070c3d5ce22ee112a5cc50c67922c07a787565
nalgaeg1t <49203563+nalgaeg1t@users.noreply.github.com>
2021-06-15 23:51:24 +0900

    Update db.js

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: '34.64.233.32',
     user: 'root',
-    password: 'process.env.DB_PASSWORD',
+    password: process.env.DB_PASSWORD,
     database: 'popic',
     port: '3306'
 });
