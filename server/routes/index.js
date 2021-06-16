35222c4b5d91fa14e026d17d9d60f90e50f7062c
nalgaeg1t <49203563+nalgaeg1t@users.noreply.github.com>
2021-06-16 20:06:48 +0900

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -22,11 +22,11 @@ router.get('/test3', authUtil, function(req, res, next) {
 })
 
 router.get('/educlass/create', function(req, res, next) {
-  let code = Math.random().toString(36).substr(2, 5) + Math.random().toString(36).substr(2, 5) + Math.random().toString(36).substr(2, 5) + Math.random().toString(36).substr(2, 5);
-  educlass.create(code, req.query.teacher_id, req.query.name, req.query.description, req.query.icon, (result) => {
-    console.log(result);
-  });
-  // res.json([code, req.query.teacher_id, req.query.name, req.query.description, req.query.icon]);
+  // let code = Math.random().toString(36).substr(2, 5) + Math.random().toString(36).substr(2, 5) + Math.random().toString(36).substr(2, 5) + Math.random().toString(36).substr(2, 5);
+  // educlass.create(code, req.query.teacher_id, req.query.name, req.query.description, req.query.icon, (result) => {
+  //   console.log(result);
+  // });
+  res.json([code, req.query.teacher_id, req.query.name, req.query.description, req.query.icon]);
 })
 
 router.get('/educlass/read', function(req, res, next) {
@@ -34,4 +34,5 @@ router.get('/educlass/read', function(req, res, next) {
     res.json(result);
   })
 })
+
 module.exports = router;
